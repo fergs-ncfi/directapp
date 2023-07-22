2edffa7768d67415073b157311ccc9d697508228
fergs-ncfi <fergal@ncfi.ie>
2023-07-22 14:43:21 +0100

    deleted:    application/src/videos/rg.mp4

---
+++ application/src/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "ncfiwebsite": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "ncfilambda": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
